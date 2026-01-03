d89bb99dd0ac931a5c8816c04191c781a28b1fd3
Santoszx30 <victorzinsantos100@gmail.com>
2026-01-03 00:42:49 -0300

    Refactor start experience and photo display logic

--- script.js
+++ script.js
@@ -1,92 +1,41 @@
-  const loading = document.getElementById("loading");
+  const startScreen = document.getElementById("startScreen");
 const app = document.getElementById("app");
-const mainHeart = document.getElementById("mainHeart");
-const startBtn = document.getElementById("startBtn");
+const heartStart = document.getElementById("heartStart");
 const photo = document.getElementById("photo");
-const textBox = document.getElementById("textBox");
-const music = document.getElementById("bgMusic");
-const canvas = document.getElementById("particles");
-const ctx = canvas.getContext("2d");
+const text = document.getElementById("text");
+const music = document.getElementById("music");
 
-canvas.width = innerWidth;
-canvas.height = innerHeight;
-
-/* ELOGIOS */
 const elogios = [
-  "OIIIII MINHA PRETINHA, teus olhos são meu lugar favorito 🫦",
-  "Você é minha <b style='color:#9b5cff'>calma</b> em meio ao caos",
-  "Teu sorriso tem um <b style='color:#ff5cf4'>efeito absurdo</b> em mim",
-  "Você é <b style='color:#6aa8ff'>beleza</b>, <b style='color:#ff5cf4'>força</b> e amor",
-  "Cada detalhe teu me faz sorrir sem perceber 🫦"
+  "OIIIII MINHA <b style='color:#c77dff'>PRETINHA</b>, você é meu lugar seguro 🫦",
+  "Sua <b style='color:#ff79c6'>beleza</b> desmonta qualquer defesa minha",
+  "Você é <b style='color:#6aa8ff'>força</b> quando eu fraquejo",
+  "Te amar é fácil, difícil é não sorrir pensando em você",
+  "Você é perigosa… do melhor jeito possível 🫦"
 ];
 
-/* FOTOS */
 let index = 0;
 
-/* INICIAR */
-function startExperience() {
-  music.play().catch(()=>{});
-  loading.style.display = "none";
+function start() {
+  startScreen.style.display = "none";
   app.classList.remove("hidden");
-  showPhoto();
-  startParticles();
+  music.play().catch(()=>{});
+  showNext();
 }
 
-mainHeart.onclick = startExperience;
-startBtn.onclick = startExperience;
+heartStart.onclick = start;
+document.getElementById("startText").onclick = start;
 
-/* MOSTRAR FOTO */
-function showPhoto() {
+function showNext() {
   photo.classList.remove("show");
 
   setTimeout(() => {
     photo.src = `fotos/${index + 1}.jpg`;
-    textBox.innerHTML = elogios[index % elogios.length];
+    text.innerHTML = elogios[index % elogios.length];
     photo.classList.add("show");
 
     index++;
-    if (index < 20) {
-      setTimeout(showPhoto, 4000);
+    if (index < 5) {
+      setTimeout(showNext, 4000);
     }
-  }, 500);
+  }, 600);
 }
-
-/* PARTÍCULAS */
-let particles = [];
-
-function startParticles() {
-  for (let i = 0; i < 80; i++) {
-    particles.push({
-      x: Math.random() * canvas.width,
-      y: Math.random() * canvas.height,
-      r: Math.random() * 3 + 1,
-      dx: (Math.random() - 0.5) * 1.2,
-      dy: (Math.random() - 0.5) * 1.2
-    });
-  }
-  animate();
-}
-
-function animate() {
-  ctx.clearRect(0,0,canvas.width,canvas.height);
-  particles.forEach(p => {
-    ctx.beginPath();
-    ctx.arc(p.x, p.y, p.r, 0, Math.PI*2);
-    ctx.fillStyle = "#9b5cff";
-    ctx.fill();
-    p.x += p.dx;
-    p.y += p.dy;
-  });
-  requestAnimationFrame(animate);
-}
-
-/* INTERAÇÃO */
-canvas.addEventListener("touchstart", e => {
-  particles.push({
-    x: e.touches[0].clientX,
-    y: e.touches[0].clientY,
-    r: 4,
-    dx: (Math.random()-0.5)*3,
-    dy: (Math.random()-0.5)*3
-  });
-});
